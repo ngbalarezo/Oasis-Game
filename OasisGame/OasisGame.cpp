a2a68a2d67599a9051cfcdbd7ecf7d72caef502a
Nicholas Balarezo <nbalarezo001@student.franciscan.edu>
2024-02-26 13:41:18 -0500

    Almost finished battle function, just have to work on light attack.

--- OasisGame/OasisGame.cpp
+++ OasisGame/OasisGame.cpp
@@ -6,13 +6,21 @@
 #include <vector>
 #include <string>
 #include "menu.h"
+#include "characters.h"
 
 int main()
 {
-    //game begins, main menu prints
+    //game begins, main menu prints 
+    /*
     mainMenu test;
     test.print();
+    */
 
-    //
+    //test battle feature
+    std::string playerName = "Khevsureti";
+    player player(playerName);
+    enemy zombie;
+    zombie.battle(player);
+
     return 0;
 }
--- OasisGame/characters.h
+++ OasisGame/characters.h
@@ -4,19 +4,47 @@
 
 #include <iostream>
 #include <string>
+#include <random>
 
 //parent class
 class character {
-	int health;
-	double attack;
-	double defense;
+protected:
+	//members
+	std::string name;
+	int hp;
+	double atk;
+	double def;
+public:
+	//constructor
+	//methods
+	std::string getName();//getters
+	int getHp();
+	double getAtk();
+	double getDef();
+
+	void setName(std::string name);//setters
+	void setHp(int hp);
+	void setAtk(double atk);
+	void setDef(double def);
 };
 
 //base classes
 class player : public character {
 private:
-
+	//members
+	int fp;
+	int sp;
 public:
+	//constructor
+	player(std::string name);
+	//methods
+	/**/
+	int getFp();
+	int getSp();
+	/**/
+	void setFp(int fp);
+	void setSp(int sp);
+
 };
 
 class npc : public character {
@@ -28,9 +56,28 @@ public:
 class enemy : public character {
 private:
 	//members
+	bool flees;
+
 public:
+	//constructors
+	enemy();
+
 	//methods
+	/**/
+	bool getFlees();
+	/**/
+	void setFlees(bool flees);
+	/**/
+	void printBattleStats(player &player);
+	/**/
+	int printBattleOptions();
+	/**/
+	void playerAttackTurn(int input, int& damageDone, std::default_random_engine& engine, player& player);
+	/**/
+	void enemyAttackTurn(int& damageDone, std::default_random_engine& engine, player& player);
+	/**/
 	void battle(player &player);
+
 };
 
 #endif
