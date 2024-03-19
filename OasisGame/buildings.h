d6807d3e25442b29e92e0950941c9e23e937a22e
Nicholas Balarezo <nbalarezo001@student.franciscan.edu>
2024-03-19 17:20:53 -0400

    created buildings.h and .cpp files.

--- OasisGame/buildings.h
+++ OasisGame/buildings.h
@@ -1,14 +1,1 @@
 #pragma once
-#ifndef BUILDINGS_H
-#define BUILDINGS_H
-
-#include <iostream>
-#include <string>
-
-#include "characters.h"
-#include "inventory.h"
-#include "items.h"
-
-
-
-#endif
