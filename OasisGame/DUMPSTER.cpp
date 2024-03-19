9f3dd1db9d1494bcb0485a1c4e4db18544ae0a24
sfromageot001 <sfromageot001@student.franciscan.edu>
2024-03-19 17:28:28 -0400

    added shop class and constructors.

--- OasisGame/DUMPSTER.cpp
+++ OasisGame/DUMPSTER.cpp
@@ -58,78 +58,8 @@ void menu() {
 
 //WORK AREA FOR BATTLE FUNCTION
 
-//#pragma once
-//#ifndef BUILDINGS_H
-//#define BUILDINGS_H
-//
-//#include <iostream>
-//#include <array>
-//#include <vector>
-//#include <string>
-//#include "menu.h"
-//#include "characters.h"
-//#include "inventory.h"
-//#include "items.h"
-//
-//class shop {
-//private:
-//    //each shop will sell 3 weapons, 1 armor, and 1 potion, 
-//    weapon shopWeaponSlot1;
-//    weapon shopWeaponSlot2;
-//    weapon shopWeaponSlot3;
-//    armor shopArmorSlot;
-//    potion shopPotionSlot;
-//    bool isEmpty;
-//    int shopLevel;
-//    //!FIXME: add in scroll/book item + functionality
-//
-//public:
-//    //constructors
-//    shop();
-//
-//    shop(weapon shopWeaponSlot1, weapon shopWeaponSlot2, weapon shopWeaponSlot3, armor shopArmorSlot, potion shopPotionSlot, bool isEmpty, int shopLevel);
-//
-//    //getters
-//
-//    //setters
-//
-//    //methods
-//
-//};
-//
-//#endif
 
 
-//#include "buildings.h"
-//#include "items.h"
-//
-////SHOP CLASS DEFINITIONS
-//
-////constructors
-//
-//shop::shop() {
-//    //create null item objects
-//    weapon noWeapon;
-//    armor noArmor;
-//    potion noPotion;
-//    //set defaults to null objects
-//    shopWeaponSlot1 = noWeapon;
-//    shopWeaponSlot2 = noWeapon;
-//    shopWeaponSlot3 = noWeapon;
-//    shopArmorSlot = noArmor;
-//    shopPotionSlot = noPotion;
-//    isEmpty = true;
-//    shopLevel = 0;
-//}
-//
-//shop::shop(weapon shopWeaponSlot1, weapon shopWeaponSlot2, weapon shopWeaponSlot3, armor shopArmorSlot, potion shopPotionSlot, bool isEmpty, int shopLevel) {
-//    this->shopWeaponSlot1 = shopWeaponSlot1;
-//    this->shopWeaponSlot2 = shopWeaponSlot2;
-//    this->shopWeaponSlot3 = shopWeaponSlot3;
-//    this->shopArmorSlot = shopArmorSlot;
-//    this->shopPotionSlot = shopPotionSlot;
-//    this->shopLevel = shopLevel;
-//}
 
 
 
