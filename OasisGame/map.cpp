941be44d591daa639fc26ede44653671b2364128
Matthew Suess <kimmattRX@outlook.com>
2024-04-16 12:28:56 -0400

    added getters, setters, and constructers to map.cpp

--- OasisGame/map.cpp
+++ OasisGame/map.cpp
@@ -4,6 +4,7 @@
 #include "buildings.h"
 #include "characters.h"
 #include "items.h"
+#include "map.h"
 
 //!FIXME: TRANSFER MAP DEFINITIONS TO THIS FILE
 
@@ -15,9 +16,32 @@
 //setters
 
 //MAP CLASS DEFINITIONS
-//constructors
+	//constructors
+map::map() {
+	startingMapNode = nullptr;
+	currentMapNode = nullptr;
+	prevMapNodeVisited = nullptr;
+}
+
+map::map(mapNode* startingMapNode, mapNode* currentMapNode, mapNode* prevMapNodeVisited) {
+	this->startingMapNode = startingMapNode;
+	this->currentMapNode = currentMapNode;
+	this->prevMapNodeVisited = prevMapNodeVisited;
+}
 
 //getters
+mapNode* map::getStartingMapNode() { return startingMapNode; }
+
+mapNode* map::getCurrentMapNode() { return currentMapNode; }
+
+mapNode* map::getPrevMapNodeVisited() { return prevMapNodeVisited; }
 
 //setters
+void map::setStartingMapNode(mapNode& startingMapNode) { this->startingMapNode = &startingMapNode; }
+
+void map::setCurrentMapNode(mapNode& currentMapNode) { this->currentMapNode = &currentMapNode; }
+
+void map::setPrevMapNodeVisited(mapNode& prevMapNodeVisited) { this->prevMapNodeVisited = &prevMapNodeVisited; }
+
+//methods
 
