eb4dda3a9229c7143d947cd4ad9efa9eab6c284f
ngbalarezo <143428049+ngbalarezo@users.noreply.github.com>
2024-04-18 00:35:17 -0400

    added dialogueNode, rewrote getters and setters, commented out the tree for now.

--- OasisGame/DialogueTree.cpp
+++ OasisGame/DialogueTree.cpp
@@ -1,4 +1,38 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <string>
 #include "DialogueTree.h"
+
+/*
+//RESPONSE NODE
+
+//DIALOGUE TREE DEFINITIONS
+//constructors
+dialogueTree::dialogueTree() {
+
+}
+
+dialogueTree::dialogueTree(dialogue& greeting, dialogue& current) {
+	this->greeting = &greeting;
+	this->current = &current;
+}
+
+//getters
+dialogue* dialogueTree::getGreeting() { return greeting; }
+
+dialogue* dialogueTree::getCurrentDialogue() { return current; }
+
+//setters
+void dialogueTree::setGreeting(dialogue& greeting) { this->greeting = &greeting; }
+
+void dialogueTree::setCurrentDialogue(dialogue& current) { this->current = &current; }
+
+//methods
+void dialogueTree::printDialogue() {
+	//begin dialogue with greeting
+	
+	
+}
+*/
 
--- OasisGame/DialogueTree.h
+++ OasisGame/DialogueTree.h
@@ -2,18 +2,48 @@
 #ifndef DIALOGUETREE_H
 #define DIALOGUETREE_H
 
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+/*
+class dialogue {
+	std::string content;
+	std::string option1;
+	std::string response1;
+	std::string option2;
+	std::string response2;
+	std::string option3;
+	std::string response3;
+	dialogue* left;
+	dialogue* right;
+	
+};
+
 class dialogueTree {
 private:
+	dialogue* greeting;
+	dialogue* current;
 
 public:
 	//constructors
+	dialogueTree();
+
+	dialogueTree(dialogue& greeting, dialogue& current);
 
 	//getters
+	dialogue* getGreeting();
+
+	dialogue* getCurrentDialogue();
 
 	//setters
+	void setGreeting(dialogue& greeting);
+
+	void setCurrentDialogue(dialogue& current);
 
 	//methods
+	void printDialogue();
 
 };
-
+*/
 #endif
